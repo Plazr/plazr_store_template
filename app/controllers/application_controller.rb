9b090c6a510bd56c7ccd9d9ba45f6a0ad427b292
Miguel Palhas <mpalhas@gmail.com>
2013-02-01 10:58:41 +0000

    added app controller filters, originally from PZS::ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :get_categories
+  before_filter :get_pages
+  before_filter :get_banner
+  before_filter :get_logo
 
   def get_categories
-    @categories = PZS::ProductCategory.all
+    @categories = PlazrStore::ProductCategory.all
   end
+
+  def get_pages
+    @pages = PlazrStore::Page.all
+  end
+
+  def get_categories
+    @categories = PlazrStore::ProductCategory.all
+  end
+
+  def get_banner
+    @banner = PlazrStore::Multimedium.banner
+  end
+
+  def get_logo
+    @logo = PlazrStore::Multimedium.logo
+  end
+
 end
