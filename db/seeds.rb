683a3e85552c54e06e5585f74d4d03702244b68a
Miguel Palhas <mpalhas@gmail.com>
2013-02-04 12:34:23 +0000

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+PlazrAuth::Engine.load_seed
+PlazrStore::Engine.load_seed
